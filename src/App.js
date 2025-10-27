b7cba38bd2cc75a2495debbfa11e2f163030b59f
titan josh <titan@kali.kali>
2025-10-27 08:38:20 -0400

    added routing on all components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
 import './App.css';
 import HomePage from './pages/homepage';
+import { Routes, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu/menu-item.js
+++ src/components/menu/menu-item.js
@@ -1,17 +1,30 @@
 import React from "react";
 import './menu-item.scss';
+import { useNavigate } from 'react-router-dom';
 
-const MenuItem = ({title , imageUrl, size}) =>(
-    <div className= {`menu-item ${size}`}
-    >
-        <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-        }}/>
-                <div className='content'>
-                    <h1 className='title'>{title.toUpperCase()}</h1>
-                <span className= 'subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkUrl = '' }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        // Navigate to either linkUrl or fallback to /shop/title
+        navigate(linkUrl || `/shop/${(title || '').toLowerCase()}`);
+    };
+
+    return (
+        <div
+            className={`menu-item ${size || ''}`}
+            onClick={handleClick}
+        >
+            <div
+                className='background-image'
+                style={{ backgroundImage: `url(${imageUrl})` }}
+            />
+            <div className='content'>
+                <h1 className='title'>{title && title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
             </div>
         </div>
-)
+    );
+};
 
 export default MenuItem;
